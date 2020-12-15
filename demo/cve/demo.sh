b417d8d87d237bd9182bac1a34e28144f8302a75
Sertac Ozercan <sozercan@gmail.com>
2020-12-14 20:43:55 -0800

    update

--- demo/cve/demo.sh
+++ demo/cve/demo.sh
@@ -20,21 +20,9 @@ echo
 echo "===== ENTER admin ====="
 echo
 
-# namespace exclusion?
-# pe "kubectl create ns staging"
-# pe "cat config.yaml"
-# pe "kubectl apply -f config.yaml"
-
-# pe "cat templates/allowedrepos_template.yaml"
-# pe "kubectl apply -f templates/allowedrepos_template.yaml"
-
 pe "cat templates/externalips_template.yaml"
 
 pe "kubectl apply -f templates/externalips_template.yaml"
-
-# pe "cat constraints/allowedrepos_contraint-dryrun.yaml"
-# pe "kubectl apply -f constraints/allowedrepos_constraint-dryrun.yaml"
-# pe "kubectl get k8sallowedrepos.constraints.gatekeeper.sh -o yaml"
 
 pe "cat constraints/externalips_constraint-dryrun.yaml"
 
@@ -50,17 +38,28 @@ pe "kubectl apply -f constraints/externalips_constraint.yaml"
 
 pe "kubectl apply -f bad/service.yaml"
 
-# pe "kubectl apply -f bad/service.yaml -n staging"
-
 pe "cat good/service.yaml"
 
 pe "kubectl apply -f good/service.yaml"
 
 echo "===== EXIT admin ====="
 echo
+wait
+
+clear
+p "*** MUTATION FEATURE IS CURRENTLY IN DEVELOPMENT ***"
+
+pe "cat assign/assign-externalip.yaml"
+
+pe "kubectl apply -f assign/assign-externalip.yaml"
+
+pe "kubectl apply -f bad/service.yaml"
+
+pe "kubectl get svc allowed-external-ip -o yaml"
+
+pe "kubectl get k8sexternalips.constraints.gatekeeper.sh external-ips -o yaml"
 
 kubectl delete -f constraints
 kubectl delete -f templates
 kubectl delete -f good
-kubectl delete ns staging
-kubectl delete -f config.yaml
+kubectl delete -f assign
