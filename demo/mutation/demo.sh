b597228e850cd399920424d062b1e73528497182
Sertac Ozercan <sozercan@gmail.com>
2020-12-14 17:11:06 -0800

    update

--- demo/mutation/demo.sh
+++ demo/mutation/demo.sh
@@ -6,6 +6,16 @@ clear
 
 p "*** MUTATION FEATURE IS CURRENTLY IN DEVELOPMENT ***"
 
+pe "cat templates/requiredlabels_template.yaml"
+
+pe "kubectl apply -f templates/requiredlabels_template.yaml"
+
+pe "cat constraints/requiredlabels_constraint.yaml"
+
+pe "kubectl apply -f constraints/requiredlabels_constraint.yaml"
+
+pe "kubectl create ns cloudnativekitchen"
+
 pe "cat assignmetadata/assign-label.yaml"
 
 pe "kubectl apply -f assignmetadata/assign-label.yaml"
@@ -13,6 +23,8 @@ pe "kubectl apply -f assignmetadata/assign-label.yaml"
 pe "kubectl create ns cloudnativekitchen"
 
 pe "kubectl get namespace cloudnativekitchen -o yaml"
+
+pe "kubectl get k8srequiredlabels all-must-have-owner -o yaml"
 
 p "THE END"
 
